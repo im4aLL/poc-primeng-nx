82c2a730e6cd80772574641d8672eab17b368218
Hadi <mhadi@approachci.com>
2024-01-18 18:12:14 -0700

    20.11.0

---
+++ jest.suppressor.js
@@ -0,0 +1,11 @@
+const originalConsoleError = console.error;
+console.error = function (...data) {
+    if (
+        typeof data[0]?.toString === 'function' &&
+        data[0].toString().includes('Error: Could not parse CSS stylesheet')
+    ) {
+        return;
+    }
+
+    originalConsoleError(...data);
+};
