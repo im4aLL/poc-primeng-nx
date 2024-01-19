e6efde3c4c95d9d88e027d5df25a34b242435e78
Hadi <mhadi@approachci.com>
2024-01-18 17:32:49 -0700

    @layer

--- jest-global-setup.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function globalSetup() {
-    const originalConsoleError = console.error;
-    const jsDomCssError = 'Error: Could not parse CSS stylesheet';
-    window['console'].error = (...params) => {
-        if (!params.find((param) => param.toString().includes(jsDomCssError))) {
-            originalConsoleError(...params);
-        }
-    };
-};
